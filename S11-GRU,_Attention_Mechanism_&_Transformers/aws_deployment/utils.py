14fe19cf9fa28055d4173b23ccb4b3388e3f6f88
Shilpa M <36926868+m-shilpa@users.noreply.github.com>
2020-11-06 22:01:24 +0530

    Create utils.py

---
+++ S11-GRU,_Attention_Mechanism_&_Transformers/aws_deployment/utils.py
@@ -0,0 +1,88 @@
+import torch
+import numpy as np
+def german_to_eng(source,source_mask,source_lengths, model, n=2, max_len=100, 
+                   sos_index=1, 
+                   src_eos_index=None, 
+                   trg_eos_index=None, 
+                   src_vocab=None, trg_vocab=None):
+    """Prints N examples. Assumes batch size of 1."""
+
+    SOS_TOKEN = "<s>"
+    EOS_TOKEN = "</s>"
+    
+    if src_vocab is not None and trg_vocab is not None:
+        src_eos_index = src_vocab.stoi[EOS_TOKEN]
+        trg_sos_index = trg_vocab.stoi[SOS_TOKEN]
+        trg_eos_index = trg_vocab.stoi[EOS_TOKEN]
+    else:
+        src_eos_index = None
+        trg_sos_index = 1
+        trg_eos_index = None
+    
+      
+    src = source
+    src = src[:-1] if src[0][-1] == src_eos_index else src
+    print('german to eng start')
+    result,_ = greedy_decode(
+      model, source, source_mask, source_lengths,
+      max_len=max_len, sos_index=trg_sos_index, eos_index=trg_eos_index)
+
+
+
+    return lookup_words(result, vocab=trg_vocab)
+
+def greedy_decode(model, src, src_mask, src_lengths, max_len=100, sos_index=1, eos_index=None):
+    """Greedily decode a sentence."""
+    print('greedy 1')
+    import torch
+    with torch.no_grad():
+        encoder_hidden, encoder_final = model.encode(src, src_mask, src_lengths)
+        prev_y = torch.ones(1, 1).fill_(sos_index).type_as(src)
+        trg_mask = torch.ones_like(prev_y)
+    print('greedy 2')
+    output = []
+    attention_scores = []
+    hidden = None
+
+    for i in range(max_len):
+        print('greedy 3')
+        with torch.no_grad():
+            out, hidden, pre_output = model.decode(
+              encoder_hidden, encoder_final, src_mask,
+              prev_y, trg_mask, hidden)
+
+            # we predict from the pre-output layer, which is
+            # a combination of Decoder state, prev emb, and context
+            prob = model.generator(pre_output[:, -1])
+
+        _, next_word = torch.max(prob, dim=1)
+        next_word = next_word.data.item()
+        output.append(next_word)
+        prev_y = torch.ones(1, 1).type_as(src).fill_(next_word)
+        attention_scores.append(model.decoder.attention.alphas.cpu().numpy())
+    print('greedy 4')
+    output = np.array(output)
+        
+    # cut off everything starting from </s> 
+    # (only when eos_index provided)
+    if eos_index is not None:
+        first_eos = np.where(output==eos_index)[0]
+        if len(first_eos) > 0:
+            output = output[:first_eos[0]]      
+    
+    return output, np.concatenate(attention_scores, axis=1)
+  
+
+
+
+def lookup_words(x, vocab=None):
+    if vocab is not None:
+        x = [vocab.itos[i] for i in x]
+
+    return [str(t) for t in x]
+
+def words_to_embedding(sentence,vocab):
+  words_list = sentence.split()
+  words_list.append('</s>')
+  x = [vocab.stoi[i] for i in words_list]
+  return x
